947d99f5111c4562eb81de293384bc04f5dc7c17
Elena <lelya2402@rambler.ru>
2022-08-20 14:11:06 +0300

    lecture

---
+++ lecture/lec1/Example002_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите ваше имя");
+string username=Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+Console.Write("Введите ваше имя");
+string user=Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(user);
---
+++ lecture/lec1/Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA=new Random().Next(1,10); // 1 2 3 4 ...9
+Console.WriteLine(numberA);
+int NumberB=new Random().Next(1,10); // 1 2 3 4 ...9
+Console.WriteLine(NumberB);
+int result=numberA + NumberB;
+Console.WriteLine(result);
