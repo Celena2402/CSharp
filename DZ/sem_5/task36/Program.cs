913b9c549e1010ac96c92f8297c02bc9df7283f2
Elena <lelya2402@rambler.ru>
2022-08-20 16:29:23 +0300

    DZ/task36

--- DZ/sem_5/task36/Program.cs
+++ DZ/sem_5/task36/Program.cs
@@ -4,3 +4,33 @@
     на нечётных позициях. [3, 7, 23, 12] -> 19 ; [-4, -6, 89, 6] -> 0
     (Искать по индексам)
 */
+
+Console.WriteLine("Введите количество элементов в массиве:");
+int leng=Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[leng];
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Random rnd = new Random();          //Создание объекта для генерации чисел
+        int value = rnd.Next(1,20);          //Получить случайное число
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+RandomArray(arr);
+Console.WriteLine();
+Console.WriteLine($"Числа, стоящие на нечетных позициях");
+
+int sum=0;
+for (int i=0; i<leng;i++)
+{
+    if (i%2>0)
+    {
+        sum=sum+arr[i];
+        Console.Write($" "+arr[i]+" ");
+    }   
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел ={sum}");
