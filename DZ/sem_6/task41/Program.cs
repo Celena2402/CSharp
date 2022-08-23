91e5582d192b894f0ee8cf6a83b0cc1fbf4add99
Elena <lelya2402@rambler.ru>
2022-08-23 17:14:09 +0300

    task41

---
+++ DZ/sem_6/task41/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 41: 
+   Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+   больше 0 ввёл пользователь
+   0, 7, 8, -2, -2 -> 2 ; -1, -7, 567, 89, 223-> 3
+
+*/
+
+Console.WriteLine("Сколько чисел будет вводиться? ");
+int leng=Convert.ToInt32(Console.ReadLine());
+
+int [] Array=new int[leng];
+int i=0;
+while (i<leng)
+{
+    Console.Write($"Введите {i+1} элемент = ");
+    Array[i]=int.Parse(Console.ReadLine());
+    i++;
+}
+Console.Write($"Ввели {leng} таких чисел: ");
+
+//вывод содержимого массива
+for (i=0; i<leng; i++)
+{
+    Console.Write($" "+ Array[i]+ " ");
+}
+Console.WriteLine();
+
+int kol=0;
+for (i=0; i<leng; i++)
+{
+    if (Array[i]>0)
+    {
+        kol=kol+1;
+    }
+}
+Console.WriteLine($"Количество положительных чисел = {kol}");
+
+
+/*
+void VvodNumber(int leng)
+{
+int i=0;
+int[] Array=new int[leng];
+while (i<leng)
+{
+    Console.Write($"Введите {i} элемент = ");
+    Array[i]=int.Parse(Console.ReadLine());
+    i++;
+}
+
+}
+
+void PrintArray(int[] Array)
+{
+    for (i=0; i<leng; i++)
+    {
+        Console.Write($" "+ Array[i]+ " ");
+    }
+}
+
+
+int [] Array=new VvodNumber[leng];
+Console.Write($"Ввели {leng} таких чисел: ");
+PrintArray(Array);
+*/
