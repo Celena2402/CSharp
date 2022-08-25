3f2c3613a00d73dabe90c8061218f1038d895cdd
Elena <lelya2402@rambler.ru>
2022-08-25 20:54:41 +0300

    task48

--- seminar/sem_6/task42/Program.cs
+++ seminar/sem_6/task42/Program.cs
@@ -76,5 +76,19 @@ int TenToTwo(int number)
 Console.WriteLine(TenToTwo(A));
 
 */
+/*
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string c = "";
 
+string TenToTwo(int number, string s)
+{
+  s = number % 2 + s;
+  if (number > 1) s = TenToTwo(number /= 2, s);
+  return s;
+}
+c = TenToTwo(a,c);
+Console.Write(c);
+
+*/
 Console.WriteLine();
