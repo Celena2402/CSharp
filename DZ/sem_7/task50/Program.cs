9174698818621f043f88f180481cc57703cdffb6
Elena <lelya2402@rambler.ru>
2022-08-27 22:03:31 +0300

    task50

--- DZ/sem_7/task50/Program.cs
+++ DZ/sem_7/task50/Program.cs
@@ -9,3 +9,65 @@
  17 -> такого числа в массиве нет
 
 */
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве= ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в массиве= ");
+int m=Convert.ToInt32(Console.ReadLine());
+
+int[,] array=NewArray(n,m,0,20);
+PrintArray(array);
+
+int[,] NewArray(int n, int m, int min, int max)
+{
+    int[,] array=new int[n,m];
+    for (int i=0; i<n;i++)
+    {
+        for (int j=0;j<m;j++)
+        {
+            array[i,j]=new Random().Next(min,max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+        {
+            Console.Write($" {array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+/*
+Console.Write("Введите нужное число = ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+Number(num);
+
+void Number(int num)
+{
+    
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0; j<array.GetLength(1);j++)
+        {
+            if (array[i,j]==num)
+            {
+                Console.WriteLine($"Это число есть в массиве.");
+            }
+            else
+            {
+                Console.WriteLine($"Такого числа в массиве нет.");
+            }
+        }
+    }
+}
+*/
