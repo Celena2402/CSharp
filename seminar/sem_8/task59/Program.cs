a08d2dc3f7445ff727fdd38d170111a36a8efb3d
Elena <lelya2402@rambler.ru>
2022-08-29 21:54:12 +0300

    sem_8/task57

--- seminar/sem_8/task59/Program.cs
+++ seminar/sem_8/task59/Program.cs
@@ -14,4 +14,51 @@
 2 2 6
 3 4 7
 */
+Console.Clear();
+Console.Write("Введите количество строк в массиве= ");
+int n=Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите количество столбцов в массиве= ");
+int m=Convert.ToInt32(Console.ReadLine());
+
+int[,] array=NewArray(n,m,1,10);
+PrintArray(array);
+Console.WriteLine();
+
+int[,] NewArray(int n, int m, int min, int max)
+{
+    int[,] array=new int[n,m];
+    for (int i=0; i<n;i++)    {
+        for (int j=0;j<m;j++)        {
+            array[i,j]=new Random().Next(min,max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)    {
+        for (int j=0;j<array.GetLength(1);j++)        {
+            Console.Write($"{array[i,j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MinNumber(int[,] array)
+{
+   // int min=0;
+    for (int i=0; i<n;i++)    {
+        for (int j=0;j<m;j++)     {
+            int min=array[1,1];
+            if (array[i,j]<min) {
+                min=array[i,j];
+            }
+        }
+    }
+    //return array;
+    Console.WriteLine($"min={min}");
+}
+
+MinNumber(array);
