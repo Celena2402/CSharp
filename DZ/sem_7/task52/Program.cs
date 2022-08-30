f9fe3b607e4c0c70849e3241aa6ca9fc85e3e10a
Elena <lelya2402@rambler.ru>
2022-08-30 10:00:18 +0300

    DZ/task52

--- DZ/sem_7/task52/Program.cs
+++ DZ/sem_7/task52/Program.cs
@@ -8,3 +8,58 @@
  8 4 2 4
  Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
 */
+Console.Clear();
+Console.Write("Введите количество строк в массиве= ");
+int m=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в массиве= ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+int[,] array=NewArray(m,n,0,10);
+PrintArray(array);
+
+int[,] NewArray(int m, int n, int min, int max)
+{
+    int[,] array=new int[m,n];
+    for (int i=0; i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            array[i,j]=new Random().Next(min,max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+        {
+            Console.Write($" {array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MeanOfColumns (int[,] array) {
+    double sum=0;
+    double result=0;
+    for (int j=0; j<array.GetLength(1);j++) 
+    {
+        for (int i=0; i<array.GetLength(0);i++) 
+        {
+            //Console.Write($" {array[i, j]} ");
+            sum=(sum+array[i,j]);        
+
+        }
+        result=Math.Round((sum/array.GetLength(0)),2);
+        Console.WriteLine($"Среднее арифметическое [{j+1}]  столбца = {result}");
+        sum=0;
+    }
+    //return sum;
+} 
+Console.WriteLine();
+MeanOfColumns(array);
+
