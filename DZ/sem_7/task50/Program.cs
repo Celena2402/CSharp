b74cf103a737d0b6d750ac4baddb8bc05c148e45
Elena <lelya2402@rambler.ru>
2022-08-30 10:25:26 +0300

    DZ/task50

--- DZ/sem_7/task50/Program.cs
+++ DZ/sem_7/task50/Program.cs
@@ -12,22 +12,14 @@
 
 */
 
-Console.Clear();
-Console.Write("Введите количество строк в массиве= ");
-int n=Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
 
-Console.Write("Введите количество столбцов в массиве= ");
-int m=Convert.ToInt32(Console.ReadLine());
-
-int[,] array=NewArray(n,m,0,20);
-PrintArray(array);
-
-int[,] NewArray(int n, int m, int min, int max)
+int[,] NewArray(int m, int n, int min, int max)
 {
-    int[,] array=new int[n,m];
-    for (int i=0; i<n;i++)
+    int[,] array=new int[m,n];
+    for (int i=0; i<m;i++)
     {
-        for (int j=0;j<m;j++)
+        for (int j=0;j<n;j++)
         {
             array[i,j]=new Random().Next(min,max);
         }
@@ -41,37 +33,53 @@ void PrintArray(int[,] array)
     {
         for (int j=0;j<array.GetLength(1);j++)
         {
-            Console.Write($" {array[i,j]} ");
+            Console.Write($" {array[i,j]} \t");
         }
         Console.WriteLine();
     }
 }
 
-Console.Write("Введите номер индекса строки в массиве = ");
+void IndexNumber(int indexI, int indexJ, int[,] array) 
+{
+    int result=0;
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0; j<array.GetLength(1);j++ )
+        {
+            if (indexI<array.GetLength(0) && indexJ<array.GetLength(1))
+            {
+                result=array[indexI,indexJ];
+            }
+            else
+            {
+                result=0;
+            }
+        }        
+    }
+    if (indexI<array.GetLength(0) && indexJ<array.GetLength(1))
+    {
+        Console.WriteLine($"Элемент массива который стоит на месте ({indexI};{indexJ}) равен {result}");
+    }
+    else
+    {
+        Console.WriteLine("Такого элемента нет и вы возможно ввели не правильное место.");
+    }
+
+}
+
+Console.Write("Введите количество строк в массиве= ");
+int m=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в массиве= ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+int[,] array=NewArray(m,n,0,20);
+PrintArray(array);
+
+Console.Write($"Введите номер индекса строки от 0 до {array.GetLength(0)-1} массивa = ");
 int indexI=Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите номер индекса столбца массива = ");
+Console.Write($"Введите номер индекса столбца от 0 до {array.GetLength(1)-1} массива = ");
 int indexJ=Convert.ToInt32(Console.ReadLine());
 
-//int[,] array=
 IndexNumber(indexI,indexJ,array);
-
-void IndexNumber(int indexI, int indexJ, int[,] array) {
-    if (indexI<array.GetLength(0) && indexJ<array.GetLength(1)){
-        Console.WriteLine($"Элемент массива который стоит на месте ({indexI};{indexJ}) равен {array[indexI,indexJ]}");
-    }
-    else{
-        Console.WriteLine("Такого элемента нет и вы возможно ввели не правильное место.");
-    }
-
-}
-/*
-int res=array[indexI,indexJ];
-
-if (indexI>=0 && indexJ>=0 && indexI<array.GetLength(0) && indexJ<array.GetLength(1)) {
-        Console.WriteLine($"Элемент массива который стоит на месте ({indexI};{indexJ}) равен {res}");
-}
-else {
-        Console.WriteLine("Такого элемента нет и вы возможно ввели не правильное место.");
-    }   
-*/
