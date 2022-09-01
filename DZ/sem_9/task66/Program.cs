25b7289f87952dfe55d171e5548200cd91d7d2b2
Elena <lelya2402@rambler.ru>
2022-09-01 22:11:51 +0300

    DZ/task66

--- DZ/sem_9/task66/Program.cs
+++ DZ/sem_9/task66/Program.cs
@@ -6,4 +6,30 @@
    M = 4; N = 8. -> 30
 */
 
+Console.Write("Введите число M= ");
+int numberM=Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число N= ");
+int numberN=Convert.ToInt32(Console.ReadLine());
+
+string NumberRec(int numberM, int numberN)
+{
+    if (numberN>numberM)
+    {
+        return $" {numberM} " + NumberRec(numberM+1, numberN);
+    }
+    else
+    {
+        return $"{numberN}";
+    }
+}
+Console.WriteLine(NumberRec(numberM,numberN));
+
+/*
+int Sum(int n)
+{
+    if(n==0) return 0;
+    else return n%10+Sum(n/10);
+}
+Console.WriteLine(Sum(n));
+*/
--- DZ/sem_9/task68/Program.cs
+++ DZ/sem_9/task68/Program.cs
@@ -5,4 +5,9 @@
    m = 2, n = 3 -> A(m,n) = 29
 */
 
+Console.Write("Введите число M= ");
+int number1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N= ");
+int number2=Convert.ToInt32(Console.ReadLine());
 
