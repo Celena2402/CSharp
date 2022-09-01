0d4f97476c441cacfa23486e9be6b7b1a1060c93
Elena <lelya2402@rambler.ru>
2022-09-01 09:29:28 +0300

    DZ/task62

--- DZ/sem_8/task62/Program.cs
+++ DZ/sem_8/task62/Program.cs
@@ -7,8 +7,8 @@
   11 16 15 06
   10 09 08 07
 */
-/*
-int[,] NewArray(int n)
+
+int[,] SpiralArray(int n)
 {
    int[,] array=new int[n,n];
    int temp=1;
@@ -17,31 +17,23 @@ int[,] NewArray(int n)
    while (temp<=array.GetLength(0)*array.GetLength(1))
    {
       array[i,j]=temp;
-      temp++;//temp=temp+1;
-      if (i<=j+1 && i+j<array.GetLength(1)-1)
-      {
-         j++; // j=j+1
-      }
+      temp++;  //temp=temp+1;
+      if (i<=j+1 && i+j<array.GetLength(1)-1) 
+          j++; // j=j+1
+      
       else
-      {
-         if (i<j && j>=array.GetLength(0)-1)
-         {
-            i++; //i=i+1
-         }
-      }
-      else
-      {
-         if (i>=j && i=j>array.GetLength(1)-1)
-         {
-            j--; //j=j-1
-         }
-      }
-      else
-      {
-         i--; //i=i-1
-      }
-   }
+      if (i<j && i+j>=array.GetLength(0)-1)
+          i++; //i=i+1
 
+      else
+      if (i>=j && i+j>array.GetLength(1)-1) 
+          j--; //j=j-1
+
+      else 
+          i--; //i=i-1
+      
+   }
+return array;
 }
 
 void PrintArray(int[,] array)
@@ -51,55 +43,24 @@ void PrintArray(int[,] array)
     {
         for (int j=0;j<array.GetLength(1);j++)
         {
-            Console.Write($"  {array[i,j]} ");
+         if (array[i,j]<10)
+         {
+            Console.Write($" 0{array[i,j]} ");
+         }
+         else
+         {
+            Console.Write($" {array[i,j]} ");
+         }         
 
         }
         Console.WriteLine();
     }
 }
 
-//Console.Write("Введите размерность квадратной маторицы: n= ");
-//int n=Convert.ToInt32(Console.ReadLine());
-int n=4;
-//int[,] array=new int[n,n];
-int[,] array=NewArray(n,n);
+Console.Write("Введите размерность квадратной матрицы: n= ");
+int n=Convert.ToInt32(Console.ReadLine());
+//int n=5;
+int[,] array=SpiralArray(n);
+//PrintArray(SpiralArray(n));
 PrintArray(array);
-*/
 
-int n = 4;
-int[,] sqareMatrix = new int[n, n];
-
-int temp = 1;
-int i = 0;
-int j = 0;
-
-while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
-{
-  sqareMatrix[i, j] = temp;
-  temp++;
-  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
-    j++;
-  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
-    i++;
-  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
-    j--;
-  else
-    i--;
-}
-
-WriteArray(sqareMatrix);
-
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (array[i,j] / 10 <= 0)
-      Console.Write($" {array[i,j]} ");
-
-      else Console.Write($"{array[i,j]} ");
-    }
-    Console.WriteLine();
-  }
-}
